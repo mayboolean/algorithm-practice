4ce6ec0db4ae5d42d633b42e74851d72f834d2a3
mayboolean <>
2024-10-17 17:45:34 -0400

    finished rotate and skyline

--- rotate_list.py
+++ rotate_list.py
@@ -1,4 +1,9 @@
 # Add your clarifying questions here
 
 def rotate_list(list, shift_by):
-    pass
+    shift_by_i = len(list) - (shift_by % len(list))
+    list = list[shift_by_i::] + list[0:shift_by_i]
+    return list
+ 
+print(rotate_list([1,2,3,4,5], 3))
+print(rotate_list(["a", "b", "c"], 1))
--- skyline.py
+++ skyline.py
@@ -1,4 +1,26 @@
 # Add your clarifying questions here
-
+# input: [-1, 1, 3, 7, 7, 3] output:[1,3,7]
+# input: [-1, 5, 3, 7, 7, 3] output:[5,7]
+# [7,7,7]
 def skyline(building_list):
-    pass
+    output = []
+    for i in range(len(building_list)):
+        if i == 0 and building_list[0]>0:
+            output.append(building_list[0])
+            continue
+        elif i == 0 and building_list[0]<0:
+            continue
+        # 
+        check_list = building_list[0:i]
+        if building_list[i] > max(check_list):
+            output.append(building_list[i])
+        elif building_list[i] == max(check_list):
+            if max(check_list) not in output:
+                output.append(building_list[i])
+        
+    return output
+
+print(skyline([-1, 1, 3, 7, 7, 3]))
+print(skyline([-1, 5, 3, 7, 7, 3]))
+print(skyline([5, 3, 7, 7, 3]))
+print(skyline([6,7,6,7,7]))
